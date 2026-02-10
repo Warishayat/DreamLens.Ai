c577f63782e567c3dfbab1ba4f0996ddf220d5a1
waris hayat <warishayat666@gmail.com>
2026-02-10 22:55:40 +0500

    Frontend Setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
-import Home from '../Pages/Home';
-import About from '../Pages/About';
-import Contact from '../Pages/Contact';
-import Login from '../Pages/Login';
-import Signup from '../Pages/Signup';
-import ImageCreation from '../Pages/ImageCreation';
-import VideoCreation from '../Pages/VideoCreation';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import Login from './Pages/Login';
+import Signup from './Pages/Signup';
+import ImageCreation from './Pages/ImageCreation';
+import VideoCreation from './Pages/VideoCreation';
 
 function App() {
   return (
