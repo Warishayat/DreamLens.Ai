197f62ffbd27de7adde2281a4acd10e960f84c40
meh5REeen <mehreens826@gmail.com>
2026-02-12 00:43:55 +0500

    contact api added

--- Backend/Routes/contact.py
+++ Backend/Routes/contact.py
@@ -1,7 +1,9 @@
 from fastapi.routing import APIRouter
 from fastapi import HTTPException,Depends
 from Validation.validation import ContactValidation
-
+from Database.database import get_db
+from Database.Schemas import ContactAdmin
+from sqlalchemy.orm import Session
 
 contact_router = APIRouter(
     prefix="/admin",
@@ -9,8 +11,38 @@ contact_router = APIRouter(
 )
 
 
-contact_router.post("/contact")
-async def ContactUs(data:ContactValidation):
-    return{
-        "message":"Contact-Form code will be work from here"
+@contact_router.post("/contact")
+async def ContactUs(data:ContactValidation,db:Session=Depends(get_db)):
+    try:
+        contact_entry = ContactAdmin(
+            name=data.name,
+            email=data.email,
+            topic=data.topic,
+            message=data.message
+
+        )
+        db.add(contact_entry)
+        db.commit()
+        db.refresh(contact_entry)
+        return {
+        "message": "Contact saved successfully!",
+        "contact_id": contact_entry.id
     }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+
+@contact_router.get('/all-contacts')
+async def getALL(db:Session=Depends(get_db)):
+    try:
+        contacts = db.query(ContactAdmin).all()
+        if contacts:
+            return{
+                "All-Contacts":contacts
+            }
+        else:
+            return {
+                "message":"No Contact Entry"
+            }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
